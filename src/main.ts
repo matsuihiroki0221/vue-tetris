5410a3728a9508d67674fa7fb1d5f973b336b8d7
h.matsui <matsui0221hiroki@gmail.com>
2022-08-18 23:05:20 +0900

    トップページ完了

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,30 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { RouteRecordRaw, createWebHistory, createRouter } from "vue-router";
+
+// トップページとゲームページを読み込む
+import TopPage from "./pages/TopPage.vue";
+import PlayPage from "./pages/PlayPage.vue";
+
+const routes: Array<RouteRecordRaw> = [
+    {
+        path: "/",
+        name: "TopPage",
+        component: TopPage,
+      },
+      {
+        path: "/play",
+        name: "PlayPage",
+        component: PlayPage
+      },
+];
+
+// 各画面のルーティング情報からルーターを作成する
+const router = createRouter({
+    history: createWebHistory(),
+    routes: routes,
+})
+
+// `use` 関数を利用してルーターを使用可能にする
+createApp(App).use(router).mount('#app')
